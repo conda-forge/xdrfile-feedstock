78cebe99526b2290574d0146d2f96c9699d58d2e
Giovanni Bussi <giovanni.bussi@gmail.com>
2021-03-01 12:58:43 +0100

    Test windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-./configure --prefix=$PREFIX --enable-shared
+if [[ "$target_platform" == "win-64" ]] ;
+then
+  ./configure --prefix=$PREFIX
+  patch_libtool
+else
+  ./configure --prefix=$PREFIX --enable-shared
+fi
+
 make -j${CPU_COUNT}
 make install
